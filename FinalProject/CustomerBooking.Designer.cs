ccc94cd3cd68ca3802eb943c9a82e1c560d53f28
Trixie Nin Cañete <tnCanete@mcm.edu.ph>
2023-10-24 21:31:10 +0800

    StretchedImage Logo

--- FinalProject/CustomerBooking.Designer.cs
+++ FinalProject/CustomerBooking.Designer.cs
@@ -92,7 +92,8 @@
             this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(104, 83);
+            this.pictureBox1.Size = new System.Drawing.Size(96, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
@@ -101,7 +102,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(122, 32);
+            this.label1.Location = new System.Drawing.Point(114, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(260, 25);
             this.label1.TabIndex = 2;
